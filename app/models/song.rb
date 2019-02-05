a98ce6a70686e17382aca8ffecbac6ada4a25a87
clifford <clifford@noukod.com>
2019-02-05 14:56:39 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def create
+    Song.create(Song_params)
+  end
+ 
+  private
+ 
+  def post_params
+    params.require(:Song).permit(:category_id, :content)
+  end
+  
 end
 
