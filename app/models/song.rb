e5742cbd2fa1a1d28dd2668e41faa0a401ea06b2
clifford <clifford@noukod.com>
2019-02-05 14:58:14 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,14 +3,30 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def create
-    Song.create(Song_params)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
-  private
+  def artist_name
+    self.artist ? self.artist.name : ''
+  end
 
-  def song_params
-    params.require(:Song).permit(:category_id, :content)
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : ''
+  end
+
+  def note_contents
+    notes.collect {|note| note.content}
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      note = self.notes.build(content: content) unless content.empty?
+    end
   end
 
 end
